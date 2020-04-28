c0702cfbd56bba6c90710d7f35f671c61f6b31b3
Nanaki <shengwanle@gmail.com>
2020-04-28 14:22:41 +0800

    add cube scene

---
+++ Assets/BioumPipeline/Editor/FogExtensionEditor.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using BioumRP;
+
+[CustomEditor(typeof(FogExtension))]
+public class BiouFogExtensionEditor : Editor
+{
+    static class Styles
+    {
+        public static GUIContent fogColor = new GUIContent("雾颜色");
+        public static GUIContent enableDistanceFog = new GUIContent("距离雾");
+        public static GUIContent distanceStart = new GUIContent("起始距离");
+        public static GUIContent distanceFalloff = new GUIContent("过度衰减");
+        public static GUIContent enableHeightFog = new GUIContent("高度雾");
+        public static GUIContent heightStart = new GUIContent("起始高度");
+        public static GUIContent heightFalloff = new GUIContent("过度衰减");
+        public static GUIContent enableSunScattering = new GUIContent("阳光散射效果");
+        public static GUIContent SunScatteringColor = new GUIContent("阳光颜色");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.Space();
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("fogColor"), Styles.fogColor);
+        EditorGUILayout.Space();
+        DistanceFogGUI();
+        EditorGUILayout.Space();
+        HeightFogGUI();
+        EditorGUILayout.Space();
+        SunScatteringGUI();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    void DistanceFogGUI()
+    {
+        SerializedProperty distanceFogToggle = serializedObject.FindProperty("distanceFog");
+        EditorGUILayout.PropertyField(distanceFogToggle, Styles.enableDistanceFog);
+
+        EditorGUI.indentLevel++;
+        if (distanceFogToggle.boolValue)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("distanceStart"), Styles.distanceStart);
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("distanceFalloff"), Styles.distanceFalloff);
+        }
+        EditorGUI.indentLevel--;
+    }
+
+    void HeightFogGUI()
+    {
+        SerializedProperty heightFogToggle = serializedObject.FindProperty("heightFog");
+        EditorGUILayout.PropertyField(heightFogToggle, Styles.enableHeightFog);
+
+        EditorGUI.indentLevel++;
+        if (heightFogToggle.boolValue)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("heightStart"), Styles.heightStart);
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("heightFalloff"), Styles.heightFalloff);
+        }
+        EditorGUI.indentLevel--;
+    }
+
+    void SunScatteringGUI()
+    {
+        SerializedProperty sunScatteringToggle = serializedObject.FindProperty("sunScattering");
+        EditorGUILayout.PropertyField(sunScatteringToggle, Styles.enableSunScattering);
+
+        EditorGUI.indentLevel++;
+        if (sunScatteringToggle.boolValue)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("SunScatteringColor"), Styles.SunScatteringColor);
+        }
+        EditorGUI.indentLevel--;
+    }
+}
---
+++ Assets/BioumPipeline/Runtime/FogExtension.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace BioumRP
+{
+    [ExecuteAlways, DisallowMultipleComponent]
+    public class FogExtension : MonoBehaviour
+    {
+        [SerializeField, ColorUsage(false, true)]
+        public Color fogColor = Color.gray;
+        [SerializeField, ColorUsage(false, true)]
+        public Color SunScatteringColor = new Color(0.9f, 0.78f, 0.51f);
+        [SerializeField]
+        public float distanceStart = 0;
+        [SerializeField, Range(0f, 1f)]
+        public float distanceFalloff = 1;
+        [SerializeField]
+        public float heightStart = 0;
+        [SerializeField, Range(0f, 2f)]
+        public float heightFalloff = 1;
+        [SerializeField]
+        public bool distanceFog = false;
+        [SerializeField]
+        public bool heightFog = false;
+        [SerializeField]
+        public bool sunScattering = false;
+
+        void OnEnable()
+        {
+            SetFogParam();
+        }
+
+        void SetFogParam()
+        {
+            float dFalloff = distanceFalloff * distanceFalloff * distanceFalloff * distanceFalloff;
+            float hFalloff = heightFalloff * heightFalloff * heightFalloff * heightFalloff;
+            Vector4 fogParam = new Vector4(distanceStart, dFalloff, heightStart, hFalloff);
+            SetFogParam(fogParam);
+        }
+
+        void SetFogParam(Vector4 fogParam)
+        {
+            RenderSettings.fog = false;
+
+            if (distanceFog)
+            {
+                Shader.EnableKeyword("BIOU_FOG_SIMPLE");
+            }
+            else
+            {
+                Shader.DisableKeyword("BIOU_FOG_SIMPLE");
+            }
+
+            if (heightFog)
+            {
+                Shader.EnableKeyword("BIOU_FOG_HEIGHT");
+            }
+            else
+            {
+                Shader.DisableKeyword("BIOU_FOG_HEIGHT");
+            }
+
+            if (sunScattering && (distanceFog || heightFog))
+            {
+                Shader.EnableKeyword("BIOU_FOG_SCATTERING");
+            }
+            else
+            {
+                Shader.DisableKeyword("BIOU_FOG_SCATTERING");
+            }
+
+            if (sunScattering && (distanceFog || heightFog))
+            {
+                //Shader.SetGlobalColor(ShaderUniforms.Scene.FogColor, fogColor);
+                //Shader.SetGlobalColor(ShaderUniforms.Scene.FogSunColor, SunScatteringColor);
+                //Shader.SetGlobalVector(ShaderUniforms.Scene.FogParam, fogParam);
+            }
+        }
+
+#if UNITY_EDITOR
+        void Update()
+        {
+            SetFogParam();
+        }
+#endif
+
+    }
+}
