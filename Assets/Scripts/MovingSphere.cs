7da4cf34c4671f5d9f04ce86f4bef084e592cb67
bioumaiquan <shengwanle@gmail.com>
2020-05-05 23:55:12 +0800

    add a sphere movement scene

---
+++ Assets/Scripts/MovingSphere.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSphere : MonoBehaviour
+{
+    [SerializeField, Range(0,100f)]
+    float maxSpeed = 10f;
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f;
+    [SerializeField, Range(0, 10)]
+    float jumpHeight = 3f;
+
+    Rigidbody body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    Vector3 velocity, desiredVelocity;
+    bool desiredJump;
+    void Update()
+    {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+
+        desiredJump |= Input.GetButtonDown("Jump");
+
+        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        velocity = body.velocity;
+        
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
+        if (desiredJump)
+        {
+            desiredJump = false;
+            Jump();
+        }
+
+        body.velocity = velocity;
+    }
+
+    void Jump()
+    {
+        velocity.y += jumpHeight;
+    }
+}
